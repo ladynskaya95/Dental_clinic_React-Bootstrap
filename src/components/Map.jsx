afba531efa0720c3f65a48ea63e92152260750db
Nastya Ladynskaya <feverrr27@gmail.com>
2022-09-22 14:07:04 +0300

    navbar services

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -9,19 +9,18 @@ const Map = () => {
   return (
     <div>
       <MapContainer
-            eventHandlers={{ DataView: () => console.log("click") }}
-            center={position}
-            zoom={12}
-            scrollWheelZoom={false}
-          >
-             <TileLayer
-            attribution='&amp;copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-            <ZoomControl position="bottomright" />
-              <Marker position={[51.505, -0.09]}/>
-            
-          </MapContainer>
+        eventHandlers={{ DataView: () => console.log("click") }}
+        center={position}
+        zoom={12}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&amp;copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <ZoomControl position="bottomright" />
+        <Marker position={[50.46, 30.49]} />
+      </MapContainer>
     </div>
   );
 }
