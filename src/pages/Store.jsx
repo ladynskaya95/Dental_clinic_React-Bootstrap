eafdd9e7a373c0b0fcd3a166d6ebe70e2b99c693
Nastya Ladynskaya <feverrr27@gmail.com>
2022-09-27 22:42:42 +0300

    store

--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
 import { Outlet, NavLink } from "react-router-dom";
-import Nav from "react-bootstrap/Nav";
+import Nav from "react-bootstrap/Nav";import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Navbar from "react-bootstrap/Navbar";
 
 const Store = () => {
   return (
     <div>
       <div className="content row">
+        <Navbar bg="light" expand="lg">
+          <Container fluid>
+            <Navbar.Brand href="#">Dental shop</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <Nav.Link href="#action1">Home</Nav.Link>
+                <Nav.Link href="#action2">Link</Nav.Link>
+                
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
         <Nav className="all_services_nav flex-column col-3">
           <Nav.Link as={NavLink} to="/services/first_appointment">
             Зубна щітка
